4f661f4e24ac27b04ec2b4abea66a9e5686ae168
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2025-10-02 16:15:01 +0000

    late push

---
+++ fst_exs/ex1_carro/Carro.java
@@ -0,0 +1,21 @@
+
+
+public class Carro{
+
+    private String modelo;
+    private String marca;
+    private int ano;
+    private int kilometers;
+    private int valor;
+    //constructor
+    public Carro(String marca, String modelo, int ano){   
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    //metodo string
+    public String toString(){
+        return "O carro eh um: " + marca + " " + modelo + ", ano " + ano;
+    }
+    
+}
---
+++ fst_exs/ex1_carro/app.java
@@ -0,0 +1,12 @@
+
+public class app{
+    public static void main(String args[]){
+
+        Carro carro1 = new Carro("carro", "bicho", 2077);
+
+        String show_carspecs = carro1.toString();
+
+        System.out.println(show_carspecs);
+
+    }
+}
