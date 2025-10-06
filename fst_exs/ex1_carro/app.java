7484a32cbef4277f0e77dab500ec5e1ba6a74a86
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2025-10-05 23:56:56 -0300

    Update app.java

--- fst_exs/ex1_carro/app.java
+++ fst_exs/ex1_carro/app.java
@@ -2,7 +2,7 @@
 public class app{
     public static void main(String args[]){
 
-        Carro carro1 = new Carro("carro", "bicho", 2077);
+        Carro carro1 = new Carro("carro", "exemplo", 2077);
 
         String show_carspecs = carro1.toString();
 
