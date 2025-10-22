9568e13969fa775dddd6a59cc103c4fbc34baf6e
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2025-10-22 22:01:12 +0000

    haha aula quarta

--- fp_array/fixacao_array.java
+++ fp_array/fixacao_array.java
@@ -1,67 +1,84 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class fixacao_array{
-    public static void main(String[] args){
+public class fixacao_array {
+    public static void main(String[] args) {
 
-        Scanner in = new Scanner (System.in);
+        Scanner in = new Scanner(System.in);
+
+        double[] sensor1 = new double[3];
+        double[] sensor2 = new double[3];
+        double[] Vresult = new double[3];
+        double[] Vconcat = new double[(sensor1.length + sensor2.length)];
+
+        double[] Vclone = new double[3];
+
         
-        double [] sensor1 = new double [3];
-        double [] sensor2 = new double [3];
-        double [] Vresult = new double [3];
-        double [] Vconcat = new double [sensor1.length + sensor2.length];
-
-
-
+        
+        
         get_temps(sensor1);
         get_temps(sensor2);
+        Vclone = clone(sensor1);
+        
         System.out.println("Temperaturas do sensor 1:");
         print_All(sensor1);
         
-        //System.out.println("\nTemperaturas do sensor 2:");
-        //print_All(sensor2);
-
-        //Vresult = get_Sub(sensor1, sensor2, Vresult);
-
-        //System.out.println("\nsubtracao 1 - 2");
-        //print_All(Vresult);
-
+        System.out.println("\nCLONE do sensor 1:");
+        print_All(Vclone);
+        
+        System.out.println("\nTemperaturas do sensor 2:");
+        print_All(sensor2);
+        
+        System.out.println("\nSUB VALOR:");
+        print_All(substituiValor(sensor1));
+        
+        // System.out.println("\nTemperaturas do sensor 2:");
+        // print_All(sensor2);
+        
+        // Vresult = get_Sub(sensor1, sensor2, Vresult);
+        
+        // System.out.println("\nsubtracao 1 - 2");.
+        // print_All(Vresult);
+        
         System.out.println("\nHAHAHA");
-        sensor1 = inverte_parimpar(sensor1);
+        // sensor1 = inverte_parimpar(sensor1);
         Vconcat = concatenar(sensor1, sensor2);
-
+        
         print_All(Vconcat);
         
+        double[] Vuniao = uniaoVet(sensor1, sensor2);
+
+        System.out.println("\nUniao dos vetores:");
+        print_All(Vuniao);
+
 
     }
 
-    
     /*
-    public static double[] get_array(double a[]){
-        Scanner in = new Scanner (System.in);
-        for (int i = 0; i < a.length; i++){
-            System.out.println("informe um numero: ");
-            a [i] = in.nextDouble();
-        }   
-        return a;
-    }
-    * 
+     * public static double[] get_array(double a[]){
+     * Scanner in = new Scanner (System.in);
+     * for (int i = 0; i < a.length; i++){
+     * System.out.println("informe um numero: ");
+     * a [i] = in.nextDouble();
+     * }
+     * return a;
+     * }
+     * 
      */
     public static double[] get_temps(double a[]) {
-       Random randomGenerator = new Random();
-       for (int i = 0; i < a.length; i++) {
-           a[i] = randomGenerator.nextInt(10000);
-       }
-       return a;
-   }
-    
-    public static double[] get_Sub(double a[], double b[], double result[]){
-        for (int i = 0; i < a.length; i++) { 
+        Random randomGenerator = new Random();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = randomGenerator.nextInt(10000);
+        }
+        return a;
+    }
+
+    public static double[] get_Sub(double a[], double b[], double result[]) {
+        for (int i = 0; i < a.length; i++) {
             result[i] = a[i] - b[i];
         }
         return result;
     }
-
 
     public static void print_All(double a[]) {
         for (int i = 0; i < a.length; i++) {
@@ -70,37 +87,71 @@ public class fixacao_array{
     }
 
     public static double[] inverte_parimpar(double a[]) {
-        double aux[] = new double [a.length];
-        for (int i = 0; i <= a.length -1 ; i++) {
+        double aux[] = new double[a.length];
+        for (int i = 0; i <= a.length - 1; i++) {
             if ((i + 1) % 2 != 0) {
-                aux[i] = a [(i+1)];
+                aux[i] = a[(i + 1)];
             }
             if ((i + 1) % 2 == 0) {
-                aux[i] = a [(i-1)];
+                aux[i] = a[(i - 1)];
             }
         }
         return aux;
     }
 
     public static double[] concatenar(double a[], double b[]) {
-        double aux[] = new double [a.length + b.length];
-        for (int i = 0; i < aux.length -1 ; i++) {
-            if (i <= a.length) {
-                aux[i] = a [i];
+        double aux[] = new double[(a.length + b.length)];
+        for (int i = 0; i < aux.length; i++) {
+            if (i < a.length) {
+                aux[i] = a[i];
             }
-            if (i > a.length && i <= b.length) {
-                aux[i] = b [(i-aux.length)];
+            if (i >= a.length) {
+                aux[i] = b[(i - a.length)];
             }
         }
         return aux;
     }
 
+    public static double[] clone(double a[]) {
+        double aux[] = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            aux[i] = a[i];
+        }
+        return aux;
+    }
 
+    public static double[] uniaoVet(double a[], double b[]) {
+        int length_count = 0;
 
+        for (int i = 0; i < a.length + b.length; i++) {
+            if (a[i] == b[i]) {
+                length_count++;
+            }
+        }
+        double aux[] = new double[length_count];
+        int j = 0;
+        for (int i = 0; i < a.length + b.length; i++) {
+            if (a[i] == b[i]) {
+                aux[j] = a[i];
+                j++;
+            }
+        }
+        return aux;
+    }
 
-
-
-
-
+    //metodo casca
+    public static double[] substituiValor(double sens[]) {
+        double impar = -2;
+        double par = -1;
+        double aux[] = new double[sens.length];
+        for (int i = 0; i <= sens.length - 1; i++) {
+            if (i % 2 == 0) {
+                aux[i] = par;
+            } else if (i % 2 == 1) {
+                aux[i] = impar;
+            }
+        }
+        return aux;
+    }
 
 }
